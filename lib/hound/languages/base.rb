69194ba0091774f4b90ce6c50b3536c2f9ec158b
Leandro Segovia <ldlsegovia@gmail.com>
2016-06-17 15:07:01 -0300

    feat(Lang::Base): check if lang is enabled before fetching rules

--- lib/hound/languages/base.rb
+++ lib/hound/languages/base.rb
@@ -4,15 +4,38 @@ module Hound
       attr_reader :rules_url, :file_name
 
       def get_rules
-        content = RestClient.get(rules_url)
-        File.write(file_path, content)
+        return false unless enabled_lang?
+        rules = get_rules_from_url
+        write_rules_to_file(rules)
         true
+      end
+
+      def hound_config
+        @hound_config ||= HoundConfig.new
       end
 
       private
 
+      def get_rules_from_url
+        RestClient.get(rules_url)
+      end
+
+      def write_rules_to_file(rules)
+        File.write(file_path, rules)
+      end
+
       def file_path
         File.join(Dir.pwd, file_name)
+      end
+
+      def enabled_lang?
+        hound_config.enabled_for?(lang_from_class)
+      end
+
+      def lang_from_class
+        lang = self.class.to_s
+        lang.slice!("Hound::Lang::")
+        lang.tableize.singularize
       end
     end
   end
--- spec/languajes/ruby_spec.rb
+++ spec/languajes/ruby_spec.rb
@@ -2,21 +2,41 @@ describe Hound::Lang::Ruby do
   subject { Hound::Lang::Ruby.new }
 
   describe "#get_rules" do
-    before do
-      stub_copy_remote_file_proccess
-      @result = subject.get_rules
+    before { stub_copy_remote_file_proccess }
+
+    context "with enabled lang" do
+      before do
+        allow_any_instance_of(HoundConfig).to receive(:enabled_for?).and_return(true)
+      end
+
+      it "gets rules from valid url" do
+        subject.get_rules
+        expect(RestClient).to have_received(:get).with(subject.rules_url).once
+      end
+
+      it "creates .rubocop.yml with files_url content" do
+        subject.get_rules
+        expect(File).to have_received(:write).with("root/#{subject.file_name}", "content").once
+      end
+
+      it "returns true after loading the rules" do
+        expect(subject.get_rules).to be_truthy
+      end
     end
 
-    it "gets rules from valid url" do
-      expect(RestClient).to have_received(:get).with(subject.rules_url).once
-    end
+    context "with disabled lang" do
+      before do
+        allow_any_instance_of(HoundConfig).to receive(:enabled_for?).and_return(false)
+      end
 
-    it "creates .rubocop.yml with files_url content" do
-      expect(File).to have_received(:write).with("root/#{subject.file_name}", "content").once
-    end
+      it "returns false" do
+        expect(subject.get_rules).to be_falsey
+      end
 
-    it "returns true after loading the rules" do
-      expect(@result).to be_truthy
+      it "tries to return config for current lang" do
+        subject.get_rules
+        expect(subject.hound_config).to have_received(:enabled_for?).with("ruby").once
+      end
     end
   end
 end
