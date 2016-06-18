b000657c1eaf1704b1de8474c05766709b6dfb8a
Leandro Segovia <ldlsegovia@gmail.com>
2016-06-18 19:04:39 -0300

    tests(): specs for merge rules ability

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,19 +1,18 @@
 RSpec.shared_examples "get rules from url" do |language, file_format|
   describe "#get_rules" do
-    let(:remote_rules) { rules(:remote, language, file_format) }
-
     def rules(type, language, file_format)
-      rules_path = Dir.pwd + "/spec/support/assets/config/rules/#{language}"
-      file = File.read("#{rules_path}/#{type}.#{file_format}")
-      processed_file = subject.serialize_rules(subject.parse_rules(file))
+      file_path = Dir.pwd + "/spec/support/assets/config/rules/#{language}/#{type}.#{file_format}"
+      content = File.read(file_path)
       {
-        original: file,
-        processed: processed_file
+        original: content,
+        path: file_path,
+        processed: subject.serialize_rules(subject.parse_rules(content))
       }
     end
 
+    let(:remote_rules) { rules(:remote, language, file_format) }
+
     before do
-      # stub copy remote file process
       allow(RestClient).to receive(:get).and_return(remote_rules[:original])
       allow(File).to receive(:write).and_return(true)
     end
@@ -28,13 +27,39 @@ RSpec.shared_examples "get rules from url" do |language, file_format|
         expect(RestClient).to have_received(:get).with(subject.rules_url).once
       end
 
-      it "creates linter file with files_url content" do
+      it "creates linter's file with files_url content" do
         subject.get_rules
-        expect(File).to have_received(:write).with("#{Dir.pwd}/#{subject.file_name}", remote_rules[:processed]).once
+        expect(File).to have_received(:write).with(
+          "#{Dir.pwd}/#{subject.file_name}", remote_rules[:processed]).once
       end
 
       it "returns true after loading the rules" do
         expect(subject.get_rules).to be_truthy
+      end
+
+      context "with defined custom rules" do
+        let(:custom_rules) { rules(:custom, language, file_format) }
+        let(:merged_rules) { rules(:merged, language, file_format) }
+
+        before do
+          allow_any_instance_of(HoundConfig).to(
+            receive(:custom_file).and_return(custom_rules[:path]))
+        end
+
+        it "creates linter's file with merged content" do
+          subject.get_rules
+          expect(File).to have_received(:write).with(
+            "#{Dir.pwd}/#{subject.file_name}", merged_rules[:processed]).once
+        end
+
+        it "returns config file from desired lang" do
+          subject.get_rules
+          expect(subject.hound_config).to have_received(:custom_file).with(language).once
+        end
+
+        it "returns true after loading the rules" do
+          expect(subject.get_rules).to be_truthy
+        end
       end
     end
 
